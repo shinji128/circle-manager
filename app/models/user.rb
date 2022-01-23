eae8c9a4a0bc8feb4acefa141fc2fa212a770d88
shinji256 <shinji65536@gmail.com>
2022-01-23 20:00:03 +0900

    LINEログイン導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  authenticates_with_sorcery!
+  has_many :authentications, :dependent => :destroy
+  accepts_nested_attributes_for :authentications
   has_one_attached :avatar
 
   include UuidGenerator
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   post 'callback' => 'users#callback'
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback"
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+  resources :users
 end
