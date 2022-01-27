a50613e9afa3cd59828b8d1269d221fc9898f76f
shinji256 <shinji65536@gmail.com>
2022-01-27 21:48:55 +0900

    Fixログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  def not_authenticated
+    flash[:warning] = t('defaults.message.require_login')
+    redirect_to auth_at_provider_path(:provider => :line)
+  end
 end
