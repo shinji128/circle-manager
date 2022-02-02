0dbf9c46c58a4ec8fa74745f760db964ebdd5959
shinji256 <shinji65536@gmail.com>
2022-02-02 20:20:21 +0900

    サークル詳細ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   post "oauth/callback" => "oauths#callback"
   get "oauth/callback" => "oauths#callback"
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
-  resources :circles, only: %i[new create]
+  resources :circles, only: %i[new create show]
 end
