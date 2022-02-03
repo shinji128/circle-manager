2798dee2a57a4ad5ca20466eff89cc4e2dbb5b74
shinji256 <shinji65536@gmail.com>
2022-02-03 19:40:46 +0900

    デフォルト画像設定

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -4,4 +4,11 @@ class Circle < ApplicationRecord
   belongs_to :user
   has_many :affiliations
 
+  before_create :default_top_image
+
+  def default_top_image
+    if !self.top_image.attached?
+      self.top_image.attach(io: File.open('app/assets/images/default_top_image.jpg'), filename: 'default_top_image.jpg')
+    end
+  end
 end
