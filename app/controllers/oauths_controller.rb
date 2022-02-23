17c90e4d628cababd6e98afcfbaef190c0e5f8b5
shinji256 <shinji65536@gmail.com>
2022-02-23 19:51:36 +0900

    redirect_to修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -9,7 +9,7 @@ class OauthsController < ApplicationController
     provider = params[:provider]
     # binding.pry
     if @user = login_from(provider)
-      redirect_back_or_to root_path, :notice => "Logged in from #{provider.titleize}!"
+      redirect_to request.referer , :notice => "Logged in from #{provider.titleize}!"
     else
       begin
         @user = create_from(provider)
@@ -17,9 +17,9 @@ class OauthsController < ApplicationController
 
         reset_session # protect from session fixation attack
         auto_login(@user)
-        redirect_back_or_to root_path, :notice => "Logged in from #{provider.titleize}!"
+        redirect_to request.referer, :notice => "Logged in from #{provider.titleize}!"
       rescue
-        redirect_back_or_to root_path, :alert => "Failed to login from #{provider.titleize}!"
+        redirect_to request.referer, :alert => "Failed to login from #{provider.titleize}!"
       end
     end
   end
