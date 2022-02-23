b420cbddf10150267f5fa7ba5b2de5044a8b3e8f
shinji256 <shinji65536@gmail.com>
2022-02-23 20:54:42 +0900

    redirect_to修正_6

--- app/controllers/affiliations_controller.rb
+++ app/controllers/affiliations_controller.rb
@@ -2,13 +2,17 @@ class AffiliationsController < ApplicationController
   def new; end
 
   def create
-    @circle = Circle.find(params[:circle_id])
-    @affiliation = @circle.affiliations.new(circle_id: @circle.id, user_id: current_user.id)
-    if !Affiliation.find_by(user_id: current_user.id, circle_id: @circle.id)
-      @affiliation.save
-      redirect_to circle_path(@circle) #フラッシュメッセージ @circle.nameに参加しました
+    if logged_in?
+      @circle = Circle.find(params[:circle_id])
+      @affiliation = @circle.affiliations.new(circle_id: @circle.id, user_id: current_user.id)
+      if !Affiliation.find_by(user_id: current_user.id, circle_id: @circle.id)
+        @affiliation.save
+        redirect_to circle_path(@circle) #フラッシュメッセージ @circle.nameに参加しました
+      else
+        redirect_to circle_path(@circle) #フラッシュメッセージ すでにメンバーです
+      end
     else
-      redirect_to circle_path(@circle) #フラッシュメッセージ すでにメンバーです
+      redirect_to auth_at_provider_path(:provider => :line)
     end
   end
 end
