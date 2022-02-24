696af2e0ef2ce384115579388c707d3c48af1c7d
shinji256 <shinji65536@gmail.com>
2022-02-24 22:37:10 +0900

    サークル設立時のバリデーション追加

--- db/migrate/20220127143711_create_circles.rb
+++ db/migrate/20220127143711_create_circles.rb
@@ -1,8 +1,8 @@
 class CreateCircles < ActiveRecord::Migration[6.1]
   def change
     create_table :circles do |t|
-      t.string :uuid
-      t.string :name
+      t.string :uuid, null: false, unique: true
+      t.string :name, null: false
       t.string :introduction
       t.references :user, null: false, foreign_key: true
 
--- db/migrate/20220127144726_create_affiliations.rb
+++ db/migrate/20220127144726_create_affiliations.rb
@@ -1,13 +1,12 @@
 class CreateAffiliations < ActiveRecord::Migration[6.1]
   def change
     create_table :affiliations do |t|
-      t.string :uuid
       t.string :introduction
-      t.integer :register_state
       t.references :user, null: false, foreign_key: true
       t.references :circle, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :affiliations, [:user_id, :circle_id], unique: true
   end
 end
