c6747d3b0d86e94694d98498fb6e3a8233d63cc7
shinji256 <shinji65536@gmail.com>
2022-02-26 15:16:50 +0900

    Userモデルにバリデーション追加

--- db/migrate/20220123093522_create_users.rb
+++ db/migrate/20220123093522_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :uuid, null: false, unique: true
-      t.string :line_user_id, null: false, unique: true
+      t.string :uuid, null: false
+      t.string :line_user_id, null: false
       t.string :name
 
       t.timestamps
     end
+    add_index :users, [:line_user_id, :uuid], unique: true
   end
 end
