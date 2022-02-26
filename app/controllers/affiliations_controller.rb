174cdf0ef4ca3007c1fcd8baeaef83f2a7ad4261
shinji256 <shinji65536@gmail.com>
2022-02-26 17:38:42 +0900

    招待リンクをuuid化

--- app/controllers/affiliations_controller.rb
+++ app/controllers/affiliations_controller.rb
@@ -5,13 +5,17 @@ class AffiliationsController < ApplicationController
 
   def create
     if logged_in?
-      @circle = Circle.find(params[:circle_id])
-      @affiliation = @circle.affiliations.new(circle_id: @circle.id, user_id: current_user.id)
-      if !Affiliation.find_by(user_id: current_user.id, circle_id: @circle.id)
-        @affiliation.save
-        redirect_to circle_path(@circle), notice: 'サークルに入会しました'
+      @circle = Circle.find_by(uuid: params[:uuid])
+      if @circle.blank?
+        redirect_to root_path, alert: '不正な招待リンクです'
       else
-        redirect_to circle_path(@circle), alert: 'あなたはすでにメンバーです'
+        @affiliation = @circle.affiliations.new(circle_id: @circle.id, user_id: current_user.id)
+        if !Affiliation.find_by(user_id: current_user.id, circle_id: @circle.id)
+          @affiliation.save
+          redirect_to circle_path(@circle), notice: 'サークルに入会しました'
+        else
+          redirect_to circle_path(@circle), alert: 'あなたはすでにメンバーです'
+        end
       end
     else
       redirect_to auth_at_provider_path(:provider => :line)
