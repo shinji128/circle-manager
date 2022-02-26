1f04039971341779d71398cc4beaae5130237a73
shinji256 <shinji65536@gmail.com>
2022-02-26 16:10:28 +0900

    Attendanceモデルにバリデーション追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,5 +2,7 @@ class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
+  validates :comment, length: { maximum: 65_535 }
+
   enum state: { present: 0, absent: 1, undecided: 2 }
 end
