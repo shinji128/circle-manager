b9e37dd9ecf50fe84776189967c94cd398524790
shinji256 <shinji65536@gmail.com>
2022-03-16 22:46:36 +0900

    全てアロー関数に変更

--- app/javascript/packs/shuffle.js
+++ app/javascript/packs/shuffle.js
@@ -19,7 +19,8 @@ if (document.URL.match(/shuffle/)) {
         arrayNumber[randomNum] = tmp;
       }
     }
-    document.querySelector('#btn-shuffle').addEventListener('click', function () {
+
+    document.querySelector('#btn-shuffle').addEventListener('click', () => {
       shuffleArray();
       console.log(arrayNumber)
     });
