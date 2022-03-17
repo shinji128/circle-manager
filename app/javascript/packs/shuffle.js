bedf5ed70be6fd14f9bf5493b278401a48edccee
shinji256 <shinji65536@gmail.com>
2022-03-17 22:00:45 +0900

    リファクタリング

--- app/javascript/packs/shuffle.js
+++ app/javascript/packs/shuffle.js
@@ -20,71 +20,33 @@ if (document.URL.match(/shuffle/)) {
       }
     }
 
-    const CourtLhUp = document.getElementById('court-lhup-1')
-    const CourtLhLo = document.getElementById('court-lhlo-1')
-    const CourtRhUp = document.getElementById('court-rhup-1')
-    const CourtRhLo = document.getElementById('court-rhlo-1')
+    const resetCourtBlock = (i) => {
+      const courtBlock = document.getElementById(`court-block-${i}`)
+      courtBlock.removeChild(courtBlock.firstChild)
+    }
 
-    const htmlCourtLhUp = (i) => {
-      const html = `<div class= "bg-blue-700 text-white rounded-full py-2 px-4 m-auto" id="court-lhup-member">
-                      <div class="text-center">${i}</div>
+    const memberHtml = (hoge) => {
+      const html = `<div class= "bg-blue-700 text-white rounded-full py-2 px-4 m-auto" id="play-member">
+                      <div class="text-center">${hoge}</div>
                     </div>`
       return html;
     }
 
-    const htmlCourtLhLo = (i) => {
-      const html = `<div class= "bg-blue-700 text-white rounded-full py-2 px-4 m-auto" id="court-lhlo-member">
-                      <div class="text-center">${i}</div>
-                    </div>`
-      return html;
-    }
-
-    const htmlCourtRhUp = (i) => {
-      const html = `<div class= "bg-blue-700 text-white rounded-full py-2 px-4 m-auto" id="court-rhup-member">
-                      <div class="text-center">${i}</div>
-                    </div>`
-      return html;
-    }
-
-    const htmlCourtRhLo = (i) => {
-      const html = `<div class= "bg-blue-700 text-white rounded-full py-2 px-4 m-auto" id="court-rhlo-member">
-                      <div class="text-center">${i}</div>
-                    </div>`
-      return html;
-    }
-
-    const BuildHtmlCourtLhUp = (i) => {
-      CourtLhUp.insertAdjacentHTML('afterend', htmlCourtLhUp(member[i].name))
-    }
-
-    const BuildHtmlCourtLhLo = (i) => {
-      CourtLhLo.insertAdjacentHTML('afterend', htmlCourtLhLo(member[i].name))
-    }
-
-    const BuildHtmlCourtRhUp = (i) => {
-      CourtRhUp.insertAdjacentHTML('afterend', htmlCourtRhUp(member[i].name))
-    }
-
-    const BuildHtmlCourtRhLo = (i) => {
-      CourtRhLo.insertAdjacentHTML('afterend', htmlCourtRhLo(member[i].name))
+    const buildMemberHtml = (i, arrayNumber) => {
+      const courtBlock = document.getElementById(`court-block-${i + 1}`)
+      courtBlock.insertAdjacentHTML('beforeend', memberHtml(member[arrayNumber].name))
     }
 
     document.querySelector('#btn-shuffle').addEventListener('click', () => {
-      const courtLhUpMember = document.getElementById('court-lhup-member')
-      const courtLhLoMember = document.getElementById('court-lhlo-member')
-      const courtRhUpMember = document.getElementById('court-rhup-member')
-      const courtRhLoMember = document.getElementById('court-rhlo-member')
-      if (courtLhUpMember) {
-        courtLhUpMember.remove();
-        courtLhLoMember.remove();
-        courtRhUpMember.remove();
-        courtRhLoMember.remove();
+      if (document.getElementById('play-member')) {
+        for (let i = 1; i < 5; i++) {
+          resetCourtBlock(i)
+        }
       }
       shuffleArray();
-      BuildHtmlCourtLhUp(arrayNumber[0]);
-      BuildHtmlCourtLhLo(arrayNumber[1]);
-      BuildHtmlCourtRhUp(arrayNumber[2]);
-      BuildHtmlCourtRhLo(arrayNumber[3]);
+      for (let i = 0; i < 4; i++) {
+        buildMemberHtml(i, arrayNumber[i])
+      }
     });
   });
 }
