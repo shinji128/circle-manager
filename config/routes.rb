27a72d58dcbea8afea74988adf5248e0b3f3b388
shinji256 <shinji65536@gmail.com>
2022-03-23 23:55:15 +0900

    マイページの編集ページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
     end
     resources :circle_roles, only: %i[new create]
   end
-  resource :mypage, only: %i[show]
+  resource :mypage, only: %i[show edit update]
 end
